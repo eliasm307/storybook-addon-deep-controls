92af501c7d342693e95a88fb81c8fb9af5081a5e
Eli <13420359+eliasm307@users.noreply.github.com>
2023-09-17 06:48:39 +0100

    add example using the addon from npm

---
+++ packages-prod/example/NOTES.md
@@ -0,0 +1,3 @@
+# Notes
+
+- Need to have this outside the workspaces directory so yarn doesnt use a link to the local version, it seems to ignore the version in the package.json
---
+++ packages-prod/example/src/stories/Dev.stories.ts
@@ -0,0 +1,105 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Dev } from "./Dev";
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  component: Dev,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+    deepControls: {
+      enabled: true,
+    },
+  },
+} satisfies Meta<typeof Dev>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Enabled: Story = {
+  args: createNestedObject(),
+};
+
+export const Disabled: Story = {
+  args: createNestedObject(),
+  parameters: {
+    deepControls: {
+      enabled: false,
+    },
+  },
+};
+
+export const WithCustomControls: Story = {
+  args: {
+    someObject: {
+      anyString: "string",
+      enumString: "string",
+    },
+  },
+  argTypes: {
+    "someObject.enumString": {
+      control: "radio",
+      options: ["value1", "value2", "value3"],
+    },
+  },
+};
+
+export const RawObject: Story = {
+  args: {
+    someObject: {
+      anyString: "string",
+      enumString: "string",
+    },
+  },
+  parameters: {
+    deepControls: {
+      enabled: false,
+    },
+  },
+};
+
+// NOTE: this doesn't include BigInt as Storybook cant serialise this
+function createNestedObject() {
+  return {
+    bool: true,
+    string: "string1234",
+    number: 1234,
+    nested: {
+      bool: false,
+      string: "string2",
+      number: 2,
+      nestedWithoutPrototype: Object.assign(Object.create(null), {
+        bool: true,
+        string: "string3",
+        element: document.createElement("span"),
+      }),
+      nullValue: null,
+      element: document.createElement("div"),
+      func: () => {},
+      nested: {
+        bool: true,
+        string: "string3",
+        number: -3,
+        nullValue: null,
+        infinity: Infinity,
+        NaNValue: NaN,
+        symbol: Symbol("symbol"),
+        classRef: class Foo {},
+        numberArray: [1, 2, 3],
+        complexArray: [
+          {
+            bool: true,
+            string: "string3",
+            number: -3,
+          },
+          document.createElement("div"),
+          null,
+          Symbol("symbol"),
+          class Bar {},
+          function () {},
+        ],
+      },
+    },
+  };
+}
