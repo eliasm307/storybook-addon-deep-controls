b64f122bb599f8b8cdf19ed54c6c2a19879102f2
Eli <13420359+eliasm307@users.noreply.github.com>
2024-08-10 22:10:54 +0200

    [Publish script] Commit changes before publishing

--- packages-prod/example/src/stories/Dev.tsx
+++ packages-prod/example/src/stories/Dev.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { stringify } from "../../../../packages/addon/src/utils/general";
+import { stringify } from "storybook-addon-deep-controls/src/utils/general";
 
 export function Dev(config: object) {
   return (
