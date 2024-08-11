f11b0f8fe965aebafa3c0499e0a97f7e4417b50a
Eli <13420359+eliasm307@users.noreply.github.com>
2024-08-11 23:14:15 +0200

    formatting

--- packages/example-v8/src/tests/utils/escapeRegExp.ts
+++ packages/example-v8/src/tests/utils/escapeRegExp.ts
@@ -8,21 +8,21 @@ const reRegExpChar = /[\\^$.*+?()[\]{}|]/g;
 const reHasRegExpChar = RegExp(reRegExpChar.source);
 
 /**
-  * Escapes the `RegExp` special characters "^", "$", "\", ".", "*", "+",
-  * "?", "(", ")", "[", "]", "{", "}", and "|" in `string`.
-  *
-  * @since 3.0.0
-  * @category String
-  * @param {string} [string=''] The string to escape.
-  * @returns {string} Returns the escaped string.
-  * @see escape, escapeRegExp, unescape
-  * @example
-  *
-  * escapeRegExp('[lodash](https://lodash.com/)')
-  * // => '\[lodash\]\(https://lodash\.com/\)'
-  */
+ * Escapes the `RegExp` special characters "^", "$", "\", ".", "*", "+",
+ * "?", "(", ")", "[", "]", "{", "}", and "|" in `string`.
+ *
+ * @since 3.0.0
+ * @category String
+ * @param {string} [string=''] The string to escape.
+ * @returns {string} Returns the escaped string.
+ * @see escape, escapeRegExp, unescape
+ * @example
+ *
+ * escapeRegExp('[lodash](https://lodash.com/)')
+ * // => '\[lodash\]\(https://lodash\.com/\)'
+ */
 export default function escapeRegExp(string: string): string {
-    return (string && reHasRegExpChar.test(string))
-        ? string.replace(reRegExpChar, '\\$&')
-        : (string || '');
+  return string && reHasRegExpChar.test(string)
+    ? string.replace(reRegExpChar, "\\$&")
+    : string || "";
 }
