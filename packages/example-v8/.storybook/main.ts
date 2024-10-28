2c3ff0a1e1f05772a62bc3486ef06ea78d671a84
Eli <13420359+eliasm307@users.noreply.github.com>
2024-10-28 22:02:49 +0100

    refactor webpack v8 example to use generic v8 stories

--- packages/example-v8/.storybook/main.ts
+++ packages/example-v8/.storybook/main.ts
@@ -1,21 +1,13 @@
 import type {StorybookConfig} from "@storybook/nextjs";
-import {dirname, join} from "path";
 
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePathToPackage(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
-}
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../../generic-v8/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-essentials", // to get controls and docs addons and make sure we are compatible with any other essential addon
     "storybook-addon-deep-controls",
-  ].map(getAbsolutePathToPackage),
+  ],
   framework: {
-    name: getAbsolutePathToPackage("@storybook/nextjs"),
+    name: "@storybook/nextjs",
     options: {
       builder: {
         fsCache: true,
@@ -24,4 +16,5 @@ const config: StorybookConfig = {
     },
   },
 };
+
 export default config;
