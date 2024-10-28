654c9e5650ddaf1b6670a8f95e4973b868722d3d
Eli <13420359+eliasm307@users.noreply.github.com>
2024-10-28 23:40:39 +0100

    update v8 webpack project, tests not passing

--- packages/example-v8-webpack/.storybook/main.ts
+++ packages/example-v8-webpack/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type {StorybookConfig} from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../../generic-v8/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../../example-v8-generic/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-essentials", // to get controls and docs addons and make sure we are compatible with any other essential addon
     "storybook-addon-deep-controls",
