c814fab2fa48a11f1c13e3a42cd1309f1111caa0
Eli <13420359+eliasm307@users.noreply.github.com>
2025-06-08 15:35:30 +0200

    Fix #54 - Update PartialStrictInputType

--- packages/addon/index.d.ts
+++ packages/addon/index.d.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import type {BaseAnnotations, Parameters as ImportToKeep} from "@storybook/types";
+import type {BaseAnnotations, Parameters as ImportToKeep, StrictInputType} from "@storybook/types";
 
 type DummyTypeToShowImportAsUsed = ImportToKeep; // need this to keep the Parameters import to allow the declaration merge below to work
 
@@ -24,12 +24,27 @@ type DeepControlsAddonParameters = {
   enabled?: boolean;
 };
 
-/** @internal */
-export type PartialStrictInputType = Omit<Partial<StrictInputType>, "type"> & {
+/**
+ * This is to prevent auto complete being broken if type uses mapped types
+ *
+ * @see issue
+ *
+ * @internal
+ */
+type RemappedOmit<T, K extends PropertyKey> = {
+  [P in keyof T as Exclude<P, K>]: T[P];
+};
+
+/**
+ * We make this type partial because with DeepControls some normally requires properties are optional
+ *
+ * @internal
+ */
+export type PartialStrictInputType = RemappedOmit<Partial<StrictInputType>, "type"> & {
   type?: Partial<StrictInputType["type"]>;
 };
 
 export type TypeWithDeepControls<T extends Pick<BaseAnnotations, "argTypes">> = T & {
-  // custom argTypes for deep controls only
+  // custom argTypes for deep controls only, loosens the key type to allow for deep control keys
   argTypes?: Record<`${string}.${string}`, PartialStrictInputType>;
 };
